9d5bcc00283aac63dcacaa1cae479b7517eb7e3d
jonathan-sheets <70708744+jonathan-sheets@users.noreply.github.com>
2020-09-17 11:09:49 -0600

    Created pokemonList variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
-alert('Hello world');
-
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
-
+let pokemonList = [
+    { name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
+    { name: 'Venusaur', height: 20, types: ['grass', 'poison'] },
+    { name: 'Sandslash', height: 10, types: ['ground'] },
+    { name: 'Squirtle', height: 5, types: ['water'] },
+    { name: 'Noctowl', height: 16, types: ['flying', 'normal'] },
+    { name: 'Vigoroth', height: 14, types: ['normal'] }
+];
