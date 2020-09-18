0ce0a4064e2e4fbe85108e61ec806a6494633bc0
jonathan-sheets <70708744+jonathan-sheets@users.noreply.github.com>
2020-09-18 13:12:43 -0600

    Create for loop iterating pokemonLIst

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,52 @@
 let pokemonList = [
-    { name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
-    { name: 'Venusaur', height: 20, types: ['grass', 'poison'] },
-    { name: 'Sandslash', height: 10, types: ['ground'] },
-    { name: 'Squirtle', height: 5, types: ['water'] },
-    { name: 'Noctowl', height: 16, types: ['flying', 'normal'] },
-    { name: 'Vigoroth', height: 14, types: ['normal'] }
+  {
+    name: "Bulbasaur",
+    height: 0.7,
+    types: ["grass", "poison"],
+  },
+  {
+    name: "Venusaur",
+    height: 2,
+    types: ["grass", "poison"],
+  },
+  {
+    name: "Sandslash",
+    height: 1,
+    types: ["ground"],
+  },
+  {
+    name: "Squirtle",
+    height: 0.5,
+    types: ["water"],
+  },
+  {
+    name: "Noctowl",
+    height: 1.6,
+    types: ["flying", "normal"],
+  },
+  {
+    name: "Vigoroth",
+    height: 1.4,
+    types: ["normal"],
+  },
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  let size;
+  if (pokemonList[i].height > 1.7) {
+    size = '<span>"Wow, that\'s big!"</span>';
+  } else {
+    size = "";
+  }
+  document.write(
+    '<div class="box">' +
+      pokemonList[i].name +
+      " (height: " +
+      pokemonList[i].height +
+      ")" +
+      " " +
+      size +
+      " " +
+      "</div>"
+  );
+}
