c03dc96616eab4202b2e9fb99aa8397fcfeeb55e
jonathan-sheets <70708744+jonathan-sheets@users.noreply.github.com>
2020-09-25 13:16:30 -0600

    Wrap Pokemon List in IIFE
    updated forEach loops to work with IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,64 @@
-//creates pokemonList variable with multiple objects//
-let pokemonList = [
-  {
-    name: "Bulbasaur",
-    height: 0.7,
-    types: ["poison", "grass"],
-  },
-  {
-    name: "Venusaur",
-    height: 2,
-    types: ["grass", "poison"],
-  },
-  {
-    name: "Sandslash",
-    height: 1,
-    types: ["ground"],
-  },
-  {
-    name: "Squirtle",
-    height: 0.5,
-    types: ["water"],
-  },
-  {
-    name: "Noctowl",
-    height: 1.6,
-    types: ["normal", "flying"],
-  },
-  {
-    name: "Vigoroth",
-    height: 1.4,
-    types: ["normal"],
-  },
-];
+// IIFE
+let pokemonRepository = (function () {
+  //creates pokemonList variable with multiple objects//
+  let pokemonList = [
+    {
+      name: "Bulbasaur",
+      height: 0.7,
+      types: ["poison", "grass"],
+    },
+    {
+      name: "Venusaur",
+      height: 2,
+      types: ["grass", "poison"],
+    },
+    {
+      name: "Sandslash",
+      height: 1,
+      types: ["ground"],
+    },
+    {
+      name: "Squirtle",
+      height: 0.5,
+      types: ["water"],
+    },
+    {
+      name: "Noctowl",
+      height: 1.6,
+      types: ["normal", "flying"],
+    },
+    {
+      name: "Vigoroth",
+      height: 1.4,
+      types: ["normal"],
+    },
+  ];
+
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    }
+  }
+  function getAll() {
+    return pokemonList;
+  }
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: "Electrike", height: 0.6, types: ["electric"] });
+console.log(pokemonRepository.getAll());
 
 // forEach loop that iterates over each item in pokemonList variable
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function (pokemon) {
   let size;
   if (pokemon.height > 1.4) {
     size = "<br><span>This is a big Pokemon</span>";
@@ -45,8 +70,8 @@ pokemonList.forEach(function(pokemon) {
 
   //forEach loop inside above forEach loop iterating over each types array
   let color;
-  pokemon.types.forEach(function(pokemonType)  {
-    if (pokemonType == "grass")  {
+  pokemon.types.forEach(function (pokemonType) {
+    if (pokemonType == "grass") {
       color = '<span style="color:#81be71;">';
     } else if (pokemonType == "ground") {
       color = '<span style="color:#461401;">';
@@ -58,6 +83,8 @@ pokemonList.forEach(function(pokemon) {
       color = '<span style="color:#ff2222;">';
     } else if (pokemonType == "poison") {
       color = '<span style="color:#9b07ec;">';
+    } else if (pokemonType == "electric") {
+      color = '<span style="color:#ffff00;">';
     }
   });
 
@@ -69,8 +96,9 @@ pokemonList.forEach(function(pokemon) {
       "m" +
       ")" +
       " " +
-      size + '<br>'
-      + color +
+      size +
+      "<br>" +
+      color +
       pokemon.types +
       " " +
       "</div>"
