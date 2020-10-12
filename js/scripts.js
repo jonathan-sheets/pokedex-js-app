0db7dd43b9345aa5ad519f62c092a9f50e7b2511
jonathan-sheets <70708744+jonathan-sheets@users.noreply.github.com>
2020-10-12 15:38:45 -0600

    Update JQuery and functions for bootstrap

--- js/scripts.js
+++ js/scripts.js
@@ -16,14 +16,20 @@ let pokemonRepository = (function () {
   function getAll() {
     return pokemonList;
   }
-  function addListItem(pokemon = {}) {
-    let $pokemonList = $(".pokemon-list");
-    let $listItem = $("<li>");
-    let $button = $('<button class="button">' + pokemon.name + '</button>');
-    $listItem.append($button);
-    $pokemonList.append($listItem);
-    $button.on("click", function (event) {
-      showDetails(pokemon);
+  function addListItem(pokemon) {
+    pokemonRepository.loadDetails(pokemon).then(function () {
+      let $pokemonList = $(".pokemon-list");
+      let $listItem = $("<li>");
+      let $button = $('<button>' + pokemon.name + '</button>');
+      let $image = $('<img class="button-icon" alt="button image" />');
+      $image.attr("src", pokemon.imageUrl);
+      $button.addClass("btn btn-light btn-lg");
+      $button.append($image);
+      $listItem.append($button);
+      $pokemonList.append($listItem);
+      $button.on("click", function (event) {
+        showDetails(pokemon);
+      });
     });
   }
 
@@ -56,65 +62,66 @@ let pokemonRepository = (function () {
       .then(function (details) {
         // Add details to the pokemon
         pokemon.imageUrl = details.sprites.other.dream_world.front_default;
-        pokemon.imageUrlBack = details.sprites.versions['generation-v']['black-white'].animated.back_default;
+        // pokemon.imageUrlBack = details.sprites.versions['generation-v']['black-white'].animated.back_default;
         pokemon.height = details.height;
+        pokemon.weight = details.weight;
         pokemon.types = [];
         for (let i = 0; i < details.types.length; i++) {
           pokemon.types.push(details.types[i].type.name);
         }
         if (pokemon.types.includes("grass")) {
-          $('#modal-container').css("background-color",
+          $('.modal-body, .modal-header, .modal-footer').css("background-color",
             "rgb(" + 0 + "," + 211 + "," + 0 + "," + 0.75 + ")");
         } else if (pokemon.types.includes("fire")) {
-          $('#modal-container').css("background-color",
+          $('.modal-body, .modal-header, .modal-footer').css("background-color",
             "rgb(" + 255 + "," + 0 + "," + 0 + "," + 0.75 + ")");
         } else if (pokemon.types.includes("electric")) {
-          $('#modal-container').css("background-color",
+          $('.modal-body, .modal-header, .modal-footer').css("background-color",
             "rgb(" + 243 + "," + 241 + "," + 0 + "," + 0.75 + ")");
         } else if (pokemon.types.includes("poison")) {
-          $('#modal-container').css("background-color", 
+          $('.modal-body, .modal-header, .modal-footer').css("background-color", 
             "rgb(" + 137 + "," + 2 + "," + 211 + "," + 0.75 + ")");
         } else if (pokemon.types.includes("flying")) {
-          $('#modal-container').css("background-color",
+          $('.modal-body, .modal-header, .modal-footer').css("background-color",
             "rgb(" + 2 + "," + 155 + "," + 255 + "," + 0.75 + ")");
         } else if (pokemon.types.includes("water")) {
-          $('#modal-container').css("background-color",
+          $('.modal-body, .modal-header, .modal-footer').css("background-color",
             "rgb(" + 3 + "," + 52 + "," + 255 + "," + 0.75 + ")");
         } else if (pokemon.types.includes("normal")) {
-          $('#modal-container').css("background-color",
+          $('.modal-body, .modal-header, .modal-footer').css("background-color",
             "rgb(" + 255 + "," + 151 + "," + 0 + "," + 0.75 + ")");
         } else if (pokemon.types.includes("fighting")) {
-          $('#modal-container').css("background-color",
+          $('.modal-body, .modal-header, .modal-footer').css("background-color",
             "rgb(" + 242 + "," + 0 + "," + 137 + "," + 0.75 + ")");
         } else if (pokemon.types.includes("ground")) {
-          $('#modal-container').css("background-color",
+          $('.modal-body, .modal-header, .modal-footer').css("background-color",
             "rgb(" + 179 + "," + 87 + "," + 0 + "," + 0.75 + ")");
         } else if (pokemon.types.includes("rock")) {
-          $('#modal-container').css("background-color",
+          $('.modal-body, .modal-header, .modal-footer').css("background-color",
             "rgb(" + 161 + "," + 149 + "," + 137 + "," + 0.75 + ")");
         } else if (pokemon.types.includes("bug")) {
-          $('#modal-container').css("background-color",
+          $('.modal-body, .modal-header, .modal-footer').css("background-color",
             "rgb(" + 163 + "," + 88 + "," + 142 + "," + 0.75 + ")");
         } else if (pokemon.types.includes("ghost")) {
-          $('#modal-container').css("background-color",
+          $('.modal-body, .modal-header, .modal-footer').css("background-color",
             "rgb(" + 255 + "," + 255 + "," + 255 + "," + 0.75 + ")");
         } else if (pokemon.types.includes("steel")) {
-          $('#modal-container').css("background-color",
+          $('.modal-body, .modal-header, .modal-footer').css("background-color",
             "rgb(" + 132 + "," +132 + "," + 132 + "," + 0.75 + ")");
         } else if (pokemon.types.includes("psychic")) {
-          $('#modal-container').css("background-color",
+          $('.modal-body, .modal-header, .modal-footer').css("background-color",
             "rgb(" + 235 + "," + 95 + "," + 80 + "," + 0.75 + ")");
         } else if (pokemon.types.includes("ice")) {
-          $('#modal-container').css("background-color",
+          $('.modal-body, .modal-header, .modal-footer').css("background-color",
             "rgb(" + 19 + "," + 131 + "," + 255 + "," + 0.75 + ")");
         } else if (pokemon.types.includes("dragon")) {
-          $('#modal-container').css("background-color",
+          $('.modal-body, .modal-header, .modal-footer').css("background-color",
             "rgb(" + 250 + "," + 103 + "," + 44 + "," + 0.75 + ")");
         } else if (pokemon.types.includes("dark")) {
-          $('#modal-container').css("background-color",
+          $('.modal-body, .modal-header, .modal-footer').css("background-color",
             "rgb(" + 50 + "," + 50 + "," + 50 + "," + 0.75 + ")");
         } else if (pokemon.types.includes("fairy")) {
-          $('#modal-container').css("background-color",
+          $('.modal-body, .modal-header, .modal-footer').css("background-color",
             "rgb(" + 229 + "," + 131 + "," + 229 + "," + 0.75 + ")");
         }
         pokemon.abilities = [];
@@ -132,63 +139,77 @@ let pokemonRepository = (function () {
 
   // to show the modal content
   function showModal(pokemon) {
-    let $modalContainer = $("#modal-container");
+    let $modalHeader = $(".modal-header");
+    let $modalBody = $(".modal-body");
+    let $modalTitle = $(".modal-title");
+
+    $('#pokedexModal').modal('show');
     // to clear the content of the modal
-    $modalContainer.empty();
+    // $modalHeader.empty();
+    $modalTitle.empty();
+    $modalBody.empty();
     // create a div element and add class modal
-    let modal = $('<div class="modal"></div>');
+    // let modal = $('<div class="modal"></div>');
     // create close button
-    let closeButtonElement = $('<button class="modal-close">close</button>');
+    // let closeButtonElement = $('<button class="modal-close">close</button>');
     // add event listener for button click
-    closeButtonElement.on("click", hideModal);
+    // closeButtonElement.on("click", hideModa l);
     // create name element for modal content
     let nameElement = $("<h1>" + pokemon.name + "</h1>");
     // create images for modal content
     let imageElement = $('<img class="modal-img">');
     imageElement.attr("src", pokemon.imageUrl);
-    let imageElementBack = $('<img class="modal-img-back">');
-    imageElementBack.attr("src", pokemon.imageUrlBack);
+    // let imageElementBack = $('<img class="modal-img-back">');
+    // imageElementBack.attr("src", pokemon.imageUrlBack);
     // create height element for modal content
-    let heightElement = $('<p>' + 'Height: ' + pokemon.height + '</p');
+    let heightElement = $('<p>' + 'Height: ' + pokemon.height + '</p>');
+    let weightElement = $('<p>' + 'Weight: ' + pokemon.weight + '</p>');
     // put types element for modal content here
     let typesElement = $('<p>' + 'Types: ' + pokemon.types + '</p>');
     
     let abilitiesElement = $('<p>' + 'Abilities: ' + pokemon.abilities + '</p>');
 
-    modal.append(closeButtonElement);
-    modal.append(nameElement);
-    modal.append(imageElement);
-    modal.append(imageElementBack);
-    modal.append(heightElement);
-    modal.append(typesElement);
-    modal.append(abilitiesElement);
-    $modalContainer.append(modal);
-    // add class to show modal
-    $modalContainer.addClass("is-visible");
+    $modalTitle.append(nameElement);
+    $modalBody.append(imageElement);
+    $modalBody.append(heightElement);
+    $modalBody.append(weightElement);
+    $modalBody.append(typesElement);
+    $modalBody.append(abilitiesElement);
   }
+    
+  //   modal.append(closeButtonElement);
+  //   modal.append(nameElement);
+  //   modal.append(imageElement);
+  //   modal.append(imageElementBack);
+  //   modal.append(heightElement);
+  //   modal.append(typesElement);
+  //   modal.append(abilitiesElement);
+  //   $modalContainer.append(modal);
+  //   $modalContainer.addClass("is-visible");
+  // }
 
   // hide modal when close button clicked
-  function hideModal() {
-    let $modalContainer = $("#modal-container");
-    $modalContainer.removeClass("is-visible");
-  }
+  // function hideModal() {
+  //   let $modalContainer = $("#modal-container");
+  //   $modalContainer.removeClass("is-visible");
+  // }
 
-  // hide modal when escape key is pressed
-  jQuery(window).on("keydown", (e) => {
-    let $modalContainer = $("#modal-container");
-    if (e.key === "Escape" && $modalContainer.hasClass("is-visible")) {
-      hideModal();
-    }
-  });
+  // // hide modal when escape key is pressed
+  // jQuery(window).on("keydown", (e) => {
+  //   let $modalContainer = $("#modal-container");
+  //   if (e.key === "Escape" && $modalContainer.hasClass("is-visible")) {
+  //     hideModal();
+  //   }
+  // });
 
-  // hide modal if mouse clicked outside
-  let $modalContainer = document.querySelector('#modal-container');
-  $modalContainer.addEventListener("click", e => {
-    let target = e.target;
-    if (target === $modalContainer) {
-      hideModal();
-    }
-  })
+  // // hide modal if mouse clicked outside
+  // let $modalContainer = document.querySelector('#modal-container');
+  // $modalContainer.addEventListener("click", e => {
+  //   let target = e.target;
+  //   if (target === $modalContainer) {
+  //     hideModal();
+  //   }
+  // })
 
   return {
     add: add,
@@ -198,7 +219,7 @@ let pokemonRepository = (function () {
     loadDetails: loadDetails,
     showDetails: showDetails,
     showModal: showModal,
-    hideModal: hideModal,
+    // hideModal: hideModal,
   };
 })();
 
@@ -208,5 +229,3 @@ pokemonRepository.loadList().then(function () {
     pokemonRepository.addListItem(pokemon);
   });
 });
-
-
